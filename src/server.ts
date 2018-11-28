a4426aa1b768df1c8680419780a9be63c600d05b
Piotr Kosiński <piotrkosinski@gmail.com>
2018-11-28 10:25:30 +0100

    PORT

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 import * as http from 'http';
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 http.createServer(app).listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
