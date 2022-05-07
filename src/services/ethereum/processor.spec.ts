151107131cf13cc2ce53a7b1502267a744258a7e
Marcin <zolkiewski.marcin@gmail.com>
2022-05-07 22:15:03 +0200

    fixed build

--- src/services/ethereum/processor.spec.ts
+++ src/services/ethereum/processor.spec.ts
@@ -12,21 +12,19 @@ describe('Processor', () => {
   const createScheduled = (conditionBlock: number = 0) => {
     const scheduled = Mock.ofType<IScheduled>();
 
-    scheduled.setup(s => s.from).returns(() => 'Sender');
-    scheduled.setup(s => s.chainId).returns(() => 1);
-    scheduled.setup(s => s.conditionBlock).returns(() => conditionBlock);
-    scheduled.setup(s => s._id).returns(() => 'id');
+    scheduled.setup((s) => s.from).returns(() => 'Sender');
+    scheduled.setup((s) => s.chainId).returns(() => 1);
+    scheduled.setup((s) => s.conditionBlock).returns(() => conditionBlock);
+    scheduled.setup((s) => s._id).returns(() => 'id');
 
     return scheduled;
   };
 
   const createScheduledWithNonce = (nonce: number) => {
     const tx = createScheduled();
-    tx.setup(s => s.update(It.isAny())).returns(
-      () => Mock.ofType<Query<any>>().object,
-    );
+    tx.setup((s) => s.update(It.isAny())).returns(() => Mock.ofType<Query<any>>().object);
 
-    tx.setup(s => s.nonce).returns(() => nonce);
+    tx.setup((s) => s.nonce).returns(() => nonce);
 
     return tx.object;
   };
@@ -34,19 +32,17 @@ describe('Processor', () => {
   const createScheduleService = (scheduled: IScheduled[]) => {
     const scheduleService = Mock.ofType<IScheduleService>();
 
-    scheduleService
-      .setup(s => s.getPending(AssetType.Ethereum))
-      .returns(async () => scheduled);
+    scheduleService.setup((s) => s.getPending(AssetType.Ethereum)).returns(async () => scheduled);
 
     return scheduleService;
   };
 
-  it('should update conditionBlock when condition met first time', async done => {
+  it('should update conditionBlock when condition met first time', async (done) => {
     const block = 1;
     const scheduled = createScheduled();
 
     scheduled
-      .setup(s => s.update(It.isAny()))
+      .setup((s) => s.update(It.isAny()))
       .callback((update: IScheduled) => {
         expect(update.conditionBlock).toBe(block);
         done();
@@ -57,17 +53,14 @@ describe('Processor', () => {
 
     const transactionExecutor = Mock.ofType<ITransactionExecutor>();
     transactionExecutor
-      .setup(s => s.execute(It.isAny(), It.isAnyNumber()))
+      .setup((s) => s.execute(It.isAny(), It.isAnyNumber()))
       .returns(async () => ({
         status: Status.Pending,
       }));
 
-    const processor = new Processor(
-      scheduleService.object,
-      transactionExecutor.object,
-    );
+    const processor = new Processor(scheduleService.object, transactionExecutor.object);
 
-    await processor.process(block);
+    await processor.process();
   });
 
   it('should not update when condition met second time', async () => {
@@ -75,7 +68,7 @@ describe('Processor', () => {
     const scheduled = createScheduled(block);
 
     scheduled
-      .setup(s => s.update(It.isAny()))
+      .setup((s) => s.update(It.isAny()))
       .returns(() => Mock.ofType<Query<any>>().object)
       .verifiable(Times.never());
 
@@ -83,28 +76,25 @@ describe('Processor', () => {
 
     const transactionExecutor = Mock.ofType<ITransactionExecutor>();
     transactionExecutor
-      .setup(s => s.execute(It.isAny(), It.isAnyNumber()))
+      .setup((s) => s.execute(It.isAny(), It.isAnyNumber()))
       .returns(async () => ({
         status: Status.Pending,
       }));
 
-    const processor = new Processor(
-      scheduleService.object,
-      transactionExecutor.object,
-    );
+    const processor = new Processor(scheduleService.object, transactionExecutor.object);
 
-    await processor.process(block);
+    await processor.process();
 
     scheduled.verifyAll();
   });
 
-  it('should update state and transaction hash when success', async done => {
+  it('should update state and transaction hash when success', async (done) => {
     const block = 1;
     const transactionHash = '0x1234';
     const scheduled = createScheduled();
 
     scheduled
-      .setup(s => s.update(It.isAny()))
+      .setup((s) => s.update(It.isAny()))
       .callback((update: IScheduled) => {
         expect(update.status).toBe(Status.Completed);
         expect(update.transactionHash).toBe(transactionHash);
@@ -117,18 +107,15 @@ describe('Processor', () => {
 
     const transactionExecutor = Mock.ofType<ITransactionExecutor>();
     transactionExecutor
-      .setup(s => s.execute(It.isAny(), It.isAnyNumber()))
+      .setup((s) => s.execute(It.isAny(), It.isAnyNumber()))
       .returns(async () => ({
         status: Status.Completed,
         transactionHash,
       }));
 
-    const processor = new Processor(
-      scheduleService.object,
-      transactionExecutor.object,
-    );
+    const processor = new Processor(scheduleService.object, transactionExecutor.object);
 
-    await processor.process(block);
+    await processor.process();
   });
 
   it('should execute transactions from lowest nonce', async () => {
@@ -144,19 +131,16 @@ describe('Processor', () => {
 
     const transactionExecutor = Mock.ofType<ITransactionExecutor>();
     transactionExecutor
-      .setup(s => s.execute(It.isAny(), It.isAnyNumber()))
+      .setup((s) => s.execute(It.isAny(), It.isAnyNumber()))
       .callback((tx: IScheduled) => executionQueue.push(tx.nonce))
       .returns(async () => ({
         status: Status.Completed,
         transactionHash,
       }));
 
-    const processor = new Processor(
-      scheduleService.object,
-      transactionExecutor.object,
-    );
+    const processor = new Processor(scheduleService.object, transactionExecutor.object);
 
-    await processor.process(block);
+    await processor.process();
 
     expect(executionQueue[0]).toBe(tx1.nonce);
     expect(executionQueue[1]).toBe(tx2.nonce);
@@ -176,7 +160,7 @@ describe('Processor', () => {
 
     const transactionExecutor = Mock.ofType<ITransactionExecutor>();
     transactionExecutor
-      .setup(s => s.execute(tx1, It.isAnyNumber()))
+      .setup((s) => s.execute(tx1, It.isAnyNumber()))
       .callback((tx: IScheduled) => executionQueue.push(tx.nonce))
       .returns(async () => ({
         status: Status.Completed,
@@ -184,19 +168,16 @@ describe('Processor', () => {
       }));
 
     transactionExecutor
-      .setup(s => s.execute(tx2, It.isAnyNumber()))
+      .setup((s) => s.execute(tx2, It.isAnyNumber()))
       .callback((tx: IScheduled) => executionQueue.push(tx.nonce))
       .returns(async () => ({
         status: Status.Pending,
         transactionHash,
       }));
 
-    const processor = new Processor(
-      scheduleService.object,
-      transactionExecutor.object,
-    );
+    const processor = new Processor(scheduleService.object, transactionExecutor.object);
 
-    await processor.process(block);
+    await processor.process();
 
     expect(executionQueue[0]).toBe(tx1.nonce);
     expect(executionQueue[1]).toBe(tx2.nonce);
