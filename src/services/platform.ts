fb11cb3505322309896e266c271b200a1cbfe366
ongun demirag <ongun.demirag@gmail.com>
2022-05-11 10:43:33 +0300

    changes to make plafrom credits deductable

--- src/services/platform.ts
+++ src/services/platform.ts
@@ -1,20 +1,19 @@
 import { utils } from 'ethers';
 
-import { IPlatform } from '../models/Models';
+import { AssetType, IPlatform } from '../models/Models';
 import Platform from '../models/PlatformSchema';
 import { createLogger } from '../logger';
 
 const logger = createLogger('platformService');
 
 async function matchTxToPlatform(tx: string): Promise<IPlatform> {
-  logger.debug(`matchTxToPlatform, tx: ${tx}`);
   try {
     const parsed = utils.parseTransaction(tx);
     const to = parsed.to.toLowerCase();
     const data = parsed.data.toLowerCase();
     const platforms = await Platform.find();
     for (const platform of platforms) {
-      const whitelistAddresses = platform['whitelist']['ethereum'][parsed.chainId];
+      const whitelistAddresses = platform['whitelist'][AssetType.Ethereum][parsed.chainId];
       const wildcard = '*';
       const hasWildcard = Object.values(whitelistAddresses).includes(wildcard);
       if (hasWildcard) {
