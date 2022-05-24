62c6a877b245657d7c361142528267c7825e17f7
ongun demirag <ongun.demirag@gmail.com>
2022-05-24 12:01:31 +0300

    modified mail templates

--- src/constants.ts
+++ src/constants.ts
@@ -5,8 +5,16 @@ export const DAY_MILLIS = 24 * HOUR_MILLIS;
 export const ETH_DECIMALS = 18;
 export const GWEI_DECIMALS = 9;
 export enum ChainId {
+  None = -1,
   Arbitrum = 42161,
   Ethereum = 1,
-  Arbitrum_Rinkeby = 421611,
   Ropsten = 3,
+  Arbitrum_Rinkeby = 421611,
+}
+export enum BlockExplorerUrl {
+  None = '',
+  Arbitrum = 'https://arbiscan.io/',
+  Ethereum = 'https://etherscan.io/',
+  Ropsten = 'https://ropsten.etherscan.io/',
+  Arbitrum_Rinkeby = 'https://testnet.arbiscan.io/',
 }
