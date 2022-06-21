085efdb129e6ca56cbd4859b66754a9ad589bff3
ongun demirag <ongun.demirag@gmail.com>
2022-06-21 13:42:43 +0300

    Added test for getProvider util.

---
+++ src/services/ethereum/utils.test.ts
@@ -0,0 +1,17 @@
+import { getProvider } from './utils';
+import { ethers } from 'ethers';
+
+import { ARBITRUM_URI, ARBITRUM_RINKEBY_URI, ETHERUM_URI, ROPSTEN_URI } from '../../env';
+
+describe('getProvider', () => {
+  test('get RPC URL of the network based on chainID', () => {
+    // given / arrange
+    const chainID = 42161;
+
+    // when / act
+    const provider = getProvider(chainID);
+
+    // then / assert
+    expect(provider).toStrictEqual(new ethers.providers.JsonRpcProvider(ARBITRUM_URI));
+  });
+});
