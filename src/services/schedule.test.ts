64353b2c35f0b5fc9739ab15dde42a0bc808c00d
ongun demirag <ongun.demirag@gmail.com>
2022-06-21 12:00:16 +0300

    Added tests for proxy reequest

--- src/services/schedule.test.ts
+++ src/services/schedule.test.ts
@@ -64,4 +64,67 @@ describe('calculateNewStatus', () => {
       expect(newStatus).toBe(Status.PendingPayment);
     });
   });
+
+  describe('direct request', () => {
+    const isProxyRequest = false;
+
+    test('returns non Error & Draft status as it is', () => {
+      // given / arrange
+      const status = Status.Cancelled; // all status besides Error & Draft
+
+      // when / act
+      const newStatus = calculateNewStatus({
+        currentStatus: status,
+        isFreeTx: false,
+        isDraft: false,
+        isStrategyTx: false,
+        isProxyRequest,
+      });
+
+      // then / assert
+      expect(newStatus).toBe(Status.Cancelled);
+    });
+  });
+
+  describe('proxy request', () => {
+    const isProxyRequest = true;
+
+    test('returns Draft for non Strategy tx', () => {
+      // given / arrange
+      const status = Status.Pending; // all status
+
+      // when / act
+      const newStatus = calculateNewStatus({
+        currentStatus: status,
+        isFreeTx: false,
+        isDraft: true,
+        isStrategyTx: false,
+        isProxyRequest,
+      });
+
+      // then / assert
+      expect(newStatus).toBe(Status.Draft);
+    });
+  });
+
+  describe('proxy request', () => {
+    const isProxyRequest = true;
+
+    test('returns Pending for Strategy tx', () => {
+      // given / arrange
+      const status = Status.Error; // all status
+
+      // when / act
+      const newStatus = calculateNewStatus({
+        currentStatus: status,
+        isFreeTx: false,
+        isDraft: true, // can be false too
+        isStrategyTx: true,
+        isProxyRequest,
+      });
+
+      // then / assert
+      expect(newStatus).toBe(Status.Error);
+    });
+  });
 });
