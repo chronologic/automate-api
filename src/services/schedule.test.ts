c9356edd47ae6d6b89a5c15df1d73338a8f1d67c
ongun demirag <ongun.demirag@gmail.com>
2022-06-21 10:17:03 +0300

    Added direct req tests for calculating new Status

--- src/services/schedule.test.ts
+++ src/services/schedule.test.ts
@@ -22,4 +22,46 @@ describe('calculateNewStatus', () => {
       expect(newStatus).toBe(Status.Pending);
     });
   });
+
+  describe('direct request', () => {
+    const isProxyRequest = false;
+
+    test('changes Draft status to Pending for free tx', () => {
+      // given / arrange
+      const status = Status.Draft;
+
+      // when / act
+      const newStatus = calculateNewStatus({
+        currentStatus: status,
+        isFreeTx: true,
+        isDraft: false,
+        isStrategyTx: false,
+        isProxyRequest,
+      });
+
+      // then / assert
+      expect(newStatus).toBe(Status.Pending);
+    });
+  });
+
+  describe('direct request', () => {
+    const isProxyRequest = false;
+
+    test('changes Draft status to Pending Payment for non free tx', () => {
+      // given / arrange
+      const status = Status.Draft;
+
+      // when / act
+      const newStatus = calculateNewStatus({
+        currentStatus: status,
+        isFreeTx: false,
+        isDraft: false,
+        isStrategyTx: false,
+        isProxyRequest,
+      });
+
+      // then / assert
+      expect(newStatus).toBe(Status.PendingPayment);
+    });
+  });
 });
